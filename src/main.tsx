e5d417afa8554321530ad3022b1c19a8b6851d05
Omar AbedelKader <113094232+omarabedelkader@users.noreply.github.com>
2026-06-21 15:41:48 +0200

    having the god version of the icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import appIconUrl from "../build/icon.svg";
 import App from "./App";
 import "./styles.css";
+
+const favicon =
+  document.querySelector<HTMLLinkElement>("link[rel='icon']") ??
+  document.createElement("link");
+favicon.rel = "icon";
+favicon.type = "image/svg+xml";
+favicon.href = appIconUrl;
+
+if (!favicon.parentElement) {
+  document.head.appendChild(favicon);
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
