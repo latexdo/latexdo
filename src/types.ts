228cadc90c842cbbb8c2e35c589d85ec4c29921e
Omar AbedelKader <113094232+omarabedelkader@users.noreply.github.com>
2026-08-07 00:11:01 +0200

    still problem with the updater (fixes: #173)

--- src/types.ts
+++ src/types.ts
@@ -305,6 +305,7 @@ export interface UpdateInstallResult extends UpdateCheckResult {
   installerPath: string | null;
   opened: boolean;
   restartScheduled?: boolean;
+  quitScheduled?: boolean;
   manualDownload?: boolean;
 }
 
@@ -314,6 +315,7 @@ export interface UpdateDownloadProgress {
     | "downloading"
     | "verifying"
     | "opening"
+    | "installing"
     | "restarting"
     | "done"
     | "error";
